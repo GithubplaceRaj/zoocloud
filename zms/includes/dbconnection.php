88c4a6e0610760e74627c1b1a4555e6de2280d65
Rajarshi Sinha <sinharajarshi5@gmail.com>
2023-11-01 19:49:52 +0530

    first commit

---
+++ zms/includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("csit805server.database.windows.net", "admin123", "?s)&+sn5f5P4_8i123", "zoo_db");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
